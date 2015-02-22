434ff6d856edb462cdc314359eff0abc914f6177
Tom Pollard <tom.pollard.11@ucl.ac.uk>
2015-02-22 13:02:00 +0000

    add unit definitions

--- session07/solutions/uclunit/uclunit/convert.py
+++ session07/solutions/uclunit/uclunit/convert.py
@@ -8,3 +8,10 @@ import os
 class IncompatibleUnitsError(Exception):
     pass
 
+# Import the unit definitions
+def import_config():
+    config_file = 'units.yaml'
+    definitions = yaml.load(open(os.path.join(os.path.dirname(__file__),
+        config_file)))
+    return definitions
+
