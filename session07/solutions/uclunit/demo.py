1ce2c0e7a4099b0c5dc1d176c3d49164a43c941e
Tom Pollard <tom.pollard.11@ucl.ac.uk>
2015-02-24 18:43:26 +0000

    remove unit defs from demo

--- session07/solutions/uclunit/demo.py
+++ session07/solutions/uclunit/demo.py
@@ -4,14 +4,6 @@ from nose.tools import assert_raises
 from uclunit.convert import Unit, NumberUnit, IncompatibleUnitsError
 from uclunit.loadunits import *
 
-# import the unit definitions from a configuration file
-definitions = import_config()
-
-# load the units into the local namespace
-for key, subkey in definitions.iteritems():
-    a = {x: Unit(x,key,subkey[x]) for x in subkey.keys()}
-    locals().update(a)
-
 # check the example assertions
 print('\n> Checking the assertions provided in the exercise...\n')
 assert(5*meters == 0.005*kilometers)
